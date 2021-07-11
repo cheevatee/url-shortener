33584e5f6ee43d5f90414582dcaae166188bb211
cheevatee <38664398+cheevatee@users.noreply.github.com>
2021-07-11 17:11:03 +0700

    Fix incorrect if syntax to route your-url

--- url-shortener.py
+++ url-shortener.py
@@ -10,13 +10,13 @@ def home():
 
 @app.route('/your-url', methods=['GET','POST'])
 def your_url():
-    return render_template('your_url.html', code=request.form['code'])
+    if request.method == 'POST':
+        return render_template('your_url.html', code=request.form['code'])
+    else:
+        return 'This is not valid'
   
 if __name__ == '__main__':  # Script executed directly?
 #    app.run(host="0.0.0.0", port=5000, debug=True,use_reloader=True)
-    if request.method == 'POST':
-        app.run(host="0.0.0.0", port=8080, debug=True,use_reloader=True)
-    else:
-        return 'This is not valid'
+     app.run(host="0.0.0.0", port=8080, debug=True,use_reloader=True)
         
         
