699e0f0756556ada794c1b7d8299f9f312f640af
cheevatee <38664398+cheevatee@users.noreply.github.com>
2021-07-11 16:53:13 +0700

    Update request to url-shortener.py

--- url-shortener.py
+++ url-shortener.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -10,7 +10,7 @@ def home():
 
 @app.route('/your-url')
 def your_url():
-    return render_template('your_url.html')
+    return render_template('your_url.html', code=request.args['code'])
   
 if __name__ == '__main__':  # Script executed directly?
 #    app.run(host="0.0.0.0", port=5000, debug=True,use_reloader=True)
